7c618a44efe24bf4df2d94d17233d0d09f23173d
Daniel Jenson <daniel.a.jenson@gmail.com>
2024-01-30 20:25:38 +0000

    working on 2D kronecker

--- tests/test_gp.py
+++ tests/test_gp.py
@@ -10,8 +10,10 @@ def test_kronecker_approx():
     grid = build_grid([{"start": 0, "stop": 1, "num": 50}] * 2)
     var, ls = 1.0, 0.1
     L_ch = cholesky(rbf, grid, var, ls)
-    L_kr = kronecker(rbf, grid, var, ls, noise=1e-6)
-    print(jnp.max(jnp.abs(L_ch - L_kr)))
+    L_kr = kronecker(rbf, grid, var, ls, noise=1e-8)
+    # print(jnp.max(jnp.abs(L_ch - L_kr)))
+    # plt.imshow(L_ch - L_kr, cmap='inferno')
+    # plt.colorbar()
     assert jnp.allclose(L_ch, L_kr)
 
 
