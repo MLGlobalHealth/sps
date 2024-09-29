fixing variance in kernels

sps/kernels.py
@@ -82,7 +82,7 @@ def rbf(
     Returns:
         A covariance matrix.
     """
-    return jnp.sqrt(var) * jnp.exp(-l2_dist_sq(x, y) / (2 * ls**2))
+    return var * jnp.exp(-l2_dist_sq(x, y) / (2 * ls**2))
 
 
 @jit
@@ -105,9 +105,7 @@ def periodic(
         A covariance matrix.
     """
     x, y = _prepare_dims(x, y)
-    return jnp.sqrt(var) * jnp.exp(
-        -2 / ls**2 * jnp.sin(jnp.pi * jnp.abs(x - y.T) / period) ** 2
-    )
+    return var * jnp.exp(-2 / ls**2 * jnp.sin(jnp.pi * jnp.abs(x - y.T) / period) ** 2)
 
 
 @jit
@@ -150,7 +148,7 @@ def matern_1_2(
         A covariance matrix.
     """
     x, y = _prepare_dims(x, y)
-    return jnp.sqrt(var) * jnp.exp(-l2_dist(x, y) / ls)
+    return var * jnp.exp(-l2_dist(x, y) / ls)
 
 
 @jit
@@ -173,7 +171,7 @@ def matern_3_2(
     """
     d = l2_dist(x, y)
     sqrt3 = 3.0 ** (1 / 2)
-    return jnp.sqrt(var) * (1 + sqrt3 * d / ls) * jnp.exp(-sqrt3 * d / ls)
+    return var * (1 + sqrt3 * d / ls) * jnp.exp(-sqrt3 * d / ls)
 
 
 @jit
@@ -197,8 +195,4 @@ def matern_5_2(
     d = l2_dist(x, y)
     dsq = jnp.square(d)
     sqrt5 = jnp.sqrt(5.0)
-    return (
-        jnp.sqrt(var)
-        * (1 + sqrt5 * d / ls + 5 / 3 * dsq / ls**2)
-        * jnp.exp(-sqrt5 * d / ls)
-    )
+    return var * (1 + sqrt5 * d / ls + 5 / 3 * dsq / ls**2) * jnp.exp(-sqrt5 * d / ls)