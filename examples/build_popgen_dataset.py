1d67ec384e08d956f99019e0eb8c9cd70ed07d43
Daniel Jenson <daniel.a.jenson@gmail.com>
2025-07-03 10:42:14 +0100

    working on building popgen dataset

--- examples/build_popgen_dataset.py
+++ examples/build_popgen_dataset.py
@@ -2,35 +2,61 @@
 import argparse
 import sys
 
-import numpy as np
 from jax import random
 from numpy.lib.format import open_memmap
 from tqdm import tqdm
+import jax.numpy as jnp
+from pathlib import Path
+import numpy as np
 
-from sps.popgen import PopGen
+from sps.popgen import PopGen, PopGenState
 
 
 def main(args):
     rng = random.key(args.seed)
     popgen = PopGen()
-    N, B, T, C, (H, W) = args.num_batches, args.batch_size, args.num_steps, 1, args.dims
-    # NOTE: you can load this dataset with np.load(args.path, mmap_mode='r')
-    mm = open_memmap(args.path, dtype=np.float32, mode="w+", shape=(N, B, T, C, H, W))
-    for i in tqdm(range(N), unit="batches"):
-        rng_i, rng = random.split(rng)
-        prevalences, _ = popgen.simulate(
-            rng_i,
-            args.num_warmup,
-            args.num_steps,
-            args.step_interval,
-            args.batch_size,
-            args.dims,
-            wrap_edges=True,
-        )
-        mm[i] = prevalences
-        if i % args.flush_every_n == 0:
-            mm.flush()
-    print(f"Finished! Saved to {args.path}")
+    N, B, T, C, (H, W) = (
+        args.num_batches_per_state,
+        args.batch_size,
+        args.num_steps,
+        1,
+        args.dims,
+    )
+    dir = Path(args.dir)
+    population = jnp.array(args.population)
+    prevalence = jnp.zeros((B, C, H, W))
+    pbar = tqdm(total=args.num_migration * args.num_mutation * N)
+    # TODO(danj): add args for param ranges?
+    for migration in jnp.logspace(-3.3, -1.3, args.num_migration):
+        for mutation in jnp.logspace(-6, -3, args.num_mutation):
+            fname = f"popgen_migration_{migration.item()}_mutation_{mutation.item()}_population_{population.item()}.npy"
+            # NOTE: you can load this dataset with np.load(path, mmap_mode='r')
+            mm = open_memmap(
+                dir / fname,
+                dtype=np.float32,
+                mode="w+",
+                shape=(N, B, T, C, H, W),
+            )
+            offset = 0
+            last_state = PopGenState(migration, mutation, population, prevalence)
+            for i in range(args.num_batches_per_state):
+                rng, rng_i = random.split(rng)
+                prevalences, last_state = popgen.simulate(
+                    rng_i,
+                    # first time do the full warmup, thereafter when last_state
+                    # is passed, skip the next 1k to collect uncorrelated sequences
+                    num_warmup=args.num_warmup if i == 0 else 1000,
+                    num_steps=args.num_steps,
+                    step_interval=args.step_interval,
+                    batch_size=args.batch_size,
+                    dims=args.dims,
+                    wrap_edges=True,
+                    state=last_state,
+                )
+                mm[offset] = prevalences
+                mm.flush()
+                offset += 1
+                pbar.update(1)
 
 
 def parse_args(argv):
@@ -49,8 +75,26 @@ def parse_args(argv):
         "-nw",
         "--num_warmup",
         type=int,
-        default=1000,
+        default=200000,
         help="Number of warmup steps (thrown away) for each set of parameters.",
+    )
+    parser.add_argument(
+        "--population",
+        type=int,
+        default=1000,
+        help="Population per deme.",
+    )
+    parser.add_argument(
+        "--num_migration",
+        type=int,
+        default=10,
+        help="Number of migration parameters to use in default range.",
+    )
+    parser.add_argument(
+        "--num_mutation",
+        type=int,
+        default=10,
+        help="Number of mutation parameters to use in default range.",
     )
     parser.add_argument(
         "-ns",
@@ -63,29 +107,22 @@ def parse_args(argv):
         "-si",
         "--step_interval",
         type=int,
-        default=16,
+        default=10,
         help="Number of timesteps between kept steps.",
     )
     parser.add_argument(
         "-bs",
         "--batch_size",
         type=int,
-        default=32,
-        help="Batch size.",
+        default=64,
+        help="Batch size for generating data.",
     )
     parser.add_argument(
         "-nb",
-        "--num_batches",
+        "--num_batches_per_state",
         type=int,
-        default=100000,
-        help="Number of batches to generate.",
-    )
-    parser.add_argument(
-        "-f",
-        "--flush_every_n",
-        type=int,
-        default=100,
-        help="Flush batches to disk every `n`.",
+        default=5000,
+        help="Number of batches to generate per parameter state.",
     )
     parser.add_argument(
         "-d",
@@ -95,11 +132,10 @@ def parse_args(argv):
         help="Dimensions of surface.",
     )
     parser.add_argument(
-        "-p",
-        "--path",
+        "--dir",
         type=str,
-        default="popgen.npy",
-        help="Output filename.",
+        default=".",
+        help="Output dir.",
     )
     return parser.parse_args(argv[1:])
 
--- examples/visualize_popgen.py
+++ examples/visualize_popgen.py
@@ -12,8 +12,8 @@ from sps.priors import Prior
 
 def main(args):
     rng = random.key(args.seed)
-    migration = Prior("fixed", {"value": 0.01})
-    mutation = Prior("fixed", {"value": 0.001})
+    migration = Prior("fixed", {"value": 0.001})
+    mutation = Prior("fixed", {"value": 0.0001})
     population = Prior("fixed", {"value": 1000})
     batch_size, wrap_edges = 1, True
     popgen = PopGen(migration, mutation, population)
@@ -26,7 +26,7 @@ def main(args):
         args.dims,
         wrap_edges,
     )
-    path = "/tmp/popgen.gif"
+    path = "popgen.gif"
     clips = (prevalences[0, :, 0, ...] * 255.0).astype(np.uint8)
     imageio.mimsave(path, clips, duration=0.2, loop=0)
 
@@ -38,9 +38,9 @@ def parse_args(argv):
     )
     parser.add_argument("-s", "--seed", type=int, default=42)
     parser.add_argument("-nw", "--num_warmup", type=int, default=10000)
-    parser.add_argument("-ns", "--num_steps", type=int, default=32)
-    parser.add_argument("-si", "--step_interval", type=int, default=16)
-    parser.add_argument("-d", "--dims", type=list, default=[128, 128])
+    parser.add_argument("-ns", "--num_steps", type=int, default=16)
+    parser.add_argument("-si", "--step_interval", type=int, default=50)
+    parser.add_argument("-d", "--dims", nargs=2, type=int, default=[32, 32])
     return parser.parse_args(argv[1:])
 
 
